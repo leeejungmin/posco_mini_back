dbb27d7979f585be53d0d14bb0952e460afbe6e2
sey0217 <applepie0403@nate.com>
2022-06-14 21:44:51 +0900

    add shoplist

--- back/src/main/java/com/example/tasty/shop/repository/ShopListMapper.java
+++ back/src/main/java/com/example/tasty/shop/repository/ShopListMapper.java
@@ -1,4 +1,12 @@
 package com.example.tasty.shop.repository;
 
+import com.example.tasty.shop.model.ShopDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface ShopListMapper {
+
+    List<ShopDto> getShopList();
 }
