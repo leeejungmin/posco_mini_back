2311d3388305639491745bcfe09d62f5134adce7
lunastan <stanamos@naver.com>
2022-06-19 22:57:35 +0900

    socket revised

--- back/src/main/java/com/example/tasty/chat/config/WebSocketConfig.java
+++ back/src/main/java/com/example/tasty/chat/config/WebSocketConfig.java
@@ -13,15 +13,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/chatroom","/user");
-        registry.setUserDestinationPrefix("/user");
+        registry.setApplicationDestinationPrefixes("/app");// 사용자가 서버에 데이터를 보낼 주소
+        registry.enableSimpleBroker("/chatroom","/user");  //해당 api를 구독하고 있는 클라에게 메세지 전달
+        registry.setUserDestinationPrefix("/user");//클라로부터 메세지 받을 prefix
     }
 
 
     @Override
     //connection 을 맺을 때 cors허용
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/chat/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 }
